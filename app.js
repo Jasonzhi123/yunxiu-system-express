c53b8f73c005b266a8540f98a0ef864704daeff9
jasonzhi123 <1244653559@qq.com>
2019-12-10 23:33:24 +0800

    添加用户接口

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express')
+const fs = require('fs')
+const https = require('https')
 
 // 创建 express 应用
 const app = express()
 
-// 监听 / 路径的 get 请求
-app.get('/', function (req, res) {
-  res.send('hello node')
-})
+const router = require('./router')
 
-// 使 express 监听 5000 端口号发起的 http 请求
-const server = app.listen(5000, function () {
+const privateKey = fs.readFileSync('./https/jasonzhi.cn.key', 'utf8')
+const certificate = fs.readFileSync('./https/jasonzhi.cn_bundle.crt', 'utf8')
+const credentials = { key: privateKey, cert: certificate }
+const httpsServer = https.createServer(credentials, app)
+const SSLPORT = 8082
+
+app.use('/', router)
+
+httpsServer.listen(SSLPORT, function () {
+  console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT)
+})
+// 使 express 监听 3000 端口号发起的 http 请求
+const server = app.listen(3000, function () {
   const { address, port } = server.address()
   console.log('Http Server is running on http://%s:%s', address, port)
 })
---
+++ router/user.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.post('/login', function (req, res, next) {
+  console.log('/user/login', req.body)
+  res.json({
+    code: 0,
+    msg: '登录成功'
+  })
+})
+
+module.exports = router
