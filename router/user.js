d3551f82fcbba77ccf97332f2805ecf346b10c42
jasonzhi123 <1244653559@qq.com>
2019-12-11 23:05:55 +0800

    【添加用户接口】

--- router/user.js
+++ router/user.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const Result = require("../models/Result");
 const router = express.Router();
+const { login } = require('../service/user')
+const { md5 } = require('../utils')
+const { PWD_SALT } = require('../utils/constant')
+const { body, validationResult } = require('express-validator')
+const boom = require('boom')
 
-router.post("/login", function(req, res, next) {
-  console.log(req.body);
-  const username = req.body.username;
-  const password = req.body.password;
-
-  if (username === "admin" && password === "1234567") {
-    new Result("登录成功").success(res);
-  } else {
-    new Result("登录失败").fail(res);
-  }
-});
+router.post("/login",
+  [
+    body('username').isString().withMessage('username类型不正确'),
+    body('password').isString().withMessage('password类型不正确')
+  ],
+  function (req, res, next) {
+    const err = validationResult(req)
+    console.log(err);
+    if (!err.isEmpty()) {
+      const [{ msg }] = err.errors
+      next(boom.badRequest(msg))
+    } else {
+      let { username, password } = req.body
+      password = md5(`${password}${PWD_SALT}`)
+      console.log(password);
+      login(username, password).then(userInfo => {
+        console.log(userInfo);
+        if (!userInfo || userInfo.length === 0) {
+          new Result("登录失败").fail(res);
+        } else {
+          new Result("登录成功").success(res);
+        }
+      })
+    }
+  });
 
 module.exports = router;
